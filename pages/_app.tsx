992e260dacbcaf6a7169b4efc8e52a5c12ede880
T-J-D-Cavey <caveyt@hotmail.co.uk>
2023-11-21 23:06:41 +0000

    tidy up framework and set up _document head metadata

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,17 @@
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
+import Head from "next/head";
+import "../styles/globals.css";
+import type { AppProps } from "next/app";
+
+
+// We need to add a provider here to pass our global state:
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Head>
+        <title>Tim's Portfolio</title>
+      </Head>
+      <Component {...pageProps} />
+    </>
+  );
 }
--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,13 +1,19 @@
-import { Html, Head, Main, NextScript } from 'next/document'
+import { Html, Head, Main, NextScript } from "next/document";
 
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <meta
+          name="description"
+          content="Tim Cavey's Web Developer Portfolio."
+        />
+      </Head>
       <body>
         <Main />
         <NextScript />
       </body>
     </Html>
-  )
+  );
 }
