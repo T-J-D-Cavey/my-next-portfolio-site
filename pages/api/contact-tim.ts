ca5ab8941a208533f0d640c64acfa8afd5657a92
T-J-D-Cavey <caveyt@hotmail.co.uk>
2023-12-05 14:47:14 +0000

    refactor api handler code. try and fix mondodb auth issues

--- pages/api/contact-tim.ts
+++ pages/api/contact-tim.ts
@@ -1,21 +1,23 @@
 import validator from "validator";
 import { connectToDB } from "../../utils/mongo_connect";
-import type { NextApiRequest, NextApiResponse } from 'next';
+import type { NextApiRequest, NextApiResponse } from "next";
 
 type ResponseObject = {
   message: string;
-}
+};
 
-export default function handler(
+export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse<ResponseObject>
 ) {
-  if(req.method !== "POST") {
-    res.status(400).json({message: "Expected a POST request but didn't get one..."});
+  if (req.method !== "POST") {
+    res
+      .status(400)
+      .json({ message: "Expected a POST request but didn't get one..." });
     return;
   }
 
-  const {name, email, message, newsletter} = req.body;
+  const { name, email, message, newsletter } = req.body;
 
   if (
     !validator.isEmail(email) ||
@@ -25,7 +27,12 @@ export default function handler(
     message.trim() === "" ||
     typeof newsletter !== "boolean"
   ) {
-    res.status(422).json({ message: "Invalid input. Make sure name, email and message are correct." });
+    res
+      .status(422)
+      .json({
+        message:
+          "Invalid input. Make sure name, email and message are correct.",
+      });
     return;
   }
 
@@ -33,14 +40,39 @@ export default function handler(
     email: validator.escape(email),
     name: validator.escape(name),
     message: validator.escape(message),
-    newsletter: newsletter
+    newsletter: newsletter,
   };
+  const connectionString = `mongodb+srv://${process.env.MONGO_DB_USER_NAME}:${process.env.MONGO_DB_PASSWORD}@${process.env.MONGO_DB_CLUSTER_NAME}.fabaugr.mongodb.net/${process.env.MONGO_DB_DATABASE_NAME}?retryWrites=true&w=majority`;
+  let client;
+  try {
+    client = await connectToDB(connectionString);
+  } catch (error) {
+    res.status(500).json({ message: "Could not connect to database." });
+    return;
+  }
 
+  if(!client) {
+    res.status(500).json({ message: "Could not connect to database." });
+    return;
+  }
+  
+  const db = client.db();
+  let result;
+  try {
+    result = await db.collection("messages").insertOne(sanitizedData);
+  } catch (error) {
+    client.close();
+    res
+      .status(500)
+      .json({ message: "Could not store your details in our database." });
+    return;
+  }
 
+  client.close();
 
-console.log(sanitizedData);
+  res.status(201).json({
+    message: "Your message and details have been received.",
+  });
 
-
-  res.status(200).json({message: "this works!"});
   return;
 }
--- utils/mongo_connect.ts
+++ utils/mongo_connect.ts
@@ -1,13 +1,13 @@
 import { MongoClient } from "mongodb";
 
 export async function connectToDB(path: string) {
-    let client;
-    try {
-      client = new MongoClient(path);
-      await client.connect();
-      return client;
-    } catch (error) {
-        console.log(error);
-        process.exit();
-    }
+  let client;
+  try {
+    client = new MongoClient(path);
+    await client.connect();
+    return client;
+  } catch (error) {
+    console.log(error);
+    throw error;
   }
+}
