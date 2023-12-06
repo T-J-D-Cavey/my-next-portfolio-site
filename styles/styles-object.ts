4d9dcadff52994cab601683ff24be945d2d6fd26
T-J-D-Cavey <caveyt@hotmail.co.uk>
2023-12-06 17:51:45 +0000

    test background colours for projects

--- styles/styles-object.ts
+++ styles/styles-object.ts
@@ -5,7 +5,7 @@ export const modeStyles: Record<string, any> = {
       color: '#FAF9F6',
       background: '#121212',
       projectBackgroundOne: '#003b49',
-      projectBackgroundTwo: '#5d3754',
+      projectBackgroundTwo: '#333333',
       projectBackgroundThree: '#193652',
       projectBackgroundFour: '#1D2E2B',
     },
@@ -14,10 +14,10 @@ export const modeStyles: Record<string, any> = {
       secondaryButton: 'outline-dark',
       color: '#202020',
       background: '#fafafa',
-      projectBackgroundOne: '#C3E6EB',
-      projectBackgroundTwo: '#E1F8DC',
-      projectBackgroundThree: '#FFE7C7',
-      projectBackgroundFour: '#CAF1DE',
+      projectBackgroundOne: '#66CC66',
+      projectBackgroundTwo: '#4D94FF',
+      projectBackgroundThree: '#D8CCE4',
+      projectBackgroundFour: '#FFB366',
     },
   };
 
